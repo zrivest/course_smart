2b4f5d8bceeb0faceeff8797a2185052d7e032d8
zrivest <zrivest@gmail.com>
2013-11-07 09:35:26 -0600

    WIP: added seed data for Teacher and Student

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@
 require 'faker'
 # seed for Teacher
 Teacher.create(prefix: 'Mr.', first_name: 'Zachary', last_name: "Rivest", email: "zrivest@schoolio.edu", password: "123")
+Teacher.create(prefix: 'Mr.', first_name: 'Emily', last_name: "Anderson", email: "eanderson@schoolio.edu", password: "123")
+Teacher.create(prefix: 'Mr.', first_name: 'Mike', last_name: "Staszack", email: "mstaszack@schoolio.edu", password: "123")
+Teacher.create(prefix: 'Mr.', first_name: 'Ryan', last_name: "Kelly", email: "rkelley@schoolio.edu", password: "123")
+
+# seed for Student
+Student.create(first_name: 'Dave', last_name: 'Hoover', birthday: '1976-05-22')
+Student.create(first_name: 'Abi', last_name: 'Noda', birthday: '1976-05-22')
+Student.create(first_name: 'Alyssa', last_name: 'Diaz', birthday: '1976-05-22')
+Student.create(first_name: 'Mike', last_name: 'Busch', birthday: '1976-05-22')
+Student.create(first_name: 'Jonathan', last_name: 'Eyler-Werve', birthday: '1976-05-22')
+Student.create(first_name: 'K', last_name: 'Solo', birthday: '1976-05-22')
+Student.create(first_name: 'Jen', last_name: 'Myers', birthday: '1976-05-22')
+Student.create(first_name: 'Ryan', last_name: 'Briones', birthday: '1976-05-22')
+Student.create(first_name: 'Alex', last_name: 'Botsford', birthday: '1976-05-22')
+Student.create(first_name: 'Jake', last_name: 'Sikora', birthday: '1976-05-22')
