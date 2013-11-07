e5c920d04754873a9f5910cbd973319bdaa5b746
zrivest <zrivest@gmail.com>
2013-11-06 23:31:19 -0600

    setup postgres, zurb, and deleted public.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseSmart::Application.config.secret_token = '422ff5e1eb8ff18bf49cea41cfa3b3c3fd818d816bbc18d3c47e16ba52b54130b3ce94091ef83992751c2210b649fc86c32e66345517276b384f11e360a76ae2'
